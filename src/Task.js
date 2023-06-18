c4dd0741d0b39a6efa0fee47ad4e3abb027b1fd3
Dedi <dedir8361@gmail.com>
2023-06-18 21:01:41 +0700

    push

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Dedi Rosadi" // TODO: replace this
+const studentId = "FE5701086" // TODO: replace this
+
+export { url, studentName, studentId }
